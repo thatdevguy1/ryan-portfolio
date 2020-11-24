4ce4ad3391b56482f624e8cda8d623d6ffc5e87f
David Bland <davidbland@hotmail.com>
2020-11-23 19:13:44 -0500

    made changes to get it working with ghpages

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import Navbar from "./components/Navbar";
 import Portfolio from "./views/Portfolio";
 import Contact from "./views/Contact";
 import Home from "./views/Home";
-import Footer from "./components/Footer"
-import { HashRouter, Link, BrowserRouter, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import { HashRouter, Link, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <BrowserRouter basename='/'>
+  return (
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <div>
+          <Navbar />
 
-            <div className="App">
-                <div>
-                    <Navbar />
-
-                    <Switch>
-
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/contact" component={Contact} />
-                        <Route exact path="/portfolio" component={Portfolio} />
-
-                    </Switch>
-                </div>
-                <div class="container"><Footer /></div>
-
-            </div>
-        </BrowserRouter>
-
-    );
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/portfolio" component={Portfolio} />
+          </Switch>
+        </div>
+        <div class="container">
+          <Footer />
+        </div>
+      </div>
+    </HashRouter>
+  );
 }
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,52 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import * as ReactBootStrap from "react-bootstrap";
 
 export default function Navbar() {
   return (
-<ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <ReactBootStrap.Navbar.Brand href="">Ryan Browne Web Developer</ReactBootStrap.Navbar.Brand>
-  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="mr-auto">
-      <ReactBootStrap.NavDropdown title="Navigate My Site" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="/">Home</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="/Portfolio">Portfolio</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="/Contact">Contact</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="https://github.com/ryanbrowne360">Connect With Me On GitHub!</ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown>
-    </ReactBootStrap.Nav>
-    <ReactBootStrap.Nav>
-      <ReactBootStrap.Nav.Link href="https://github.com/ryanbrowne360/PortfolioPageUpdate1/blob/main/Ryan%20Browne%20Web%20Developer%20Resume%20.pdf">Please See My Resume Here</ReactBootStrap.Nav.Link>
-    </ReactBootStrap.Nav>
-  </ReactBootStrap.Navbar.Collapse>
-</ReactBootStrap.Navbar>
+    <ReactBootStrap.Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <ReactBootStrap.Navbar.Brand href="">
+        Ryan Browne Web Developer
+      </ReactBootStrap.Navbar.Brand>
+      <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+        <ReactBootStrap.Nav className="mr-auto">
+          <ReactBootStrap.NavDropdown
+            title="Navigate My Site"
+            id="collasible-nav-dropdown"
+          >
+            {/*WRAP LINKS IN NAVLINK AND USE to="" INSTEAD OF HREF*/}
+            <NavLink to="/">
+              <ReactBootStrap.NavDropdown.Item>
+                Home
+              </ReactBootStrap.NavDropdown.Item>
+            </NavLink>
 
-    );
+            <NavLink to="/portfolio">
+              <ReactBootStrap.NavDropdown.Item>
+                Portfolio
+              </ReactBootStrap.NavDropdown.Item>
+            </NavLink>
+            <ReactBootStrap.NavDropdown.Item href="/contact">
+              Contact
+            </ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Divider />
+            <ReactBootStrap.NavDropdown.Item href="https://github.com/ryanbrowne360">
+              Connect With Me On GitHub!
+            </ReactBootStrap.NavDropdown.Item>
+          </ReactBootStrap.NavDropdown>
+        </ReactBootStrap.Nav>
+        <ReactBootStrap.Nav>
+          <ReactBootStrap.Nav.Link href="https://github.com/ryanbrowne360/PortfolioPageUpdate1/blob/main/Ryan%20Browne%20Web%20Developer%20Resume%20.pdf">
+            Please See My Resume Here
+          </ReactBootStrap.Nav.Link>
+        </ReactBootStrap.Nav>
+      </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
+  );
 }
-
-
